8c8c7619b2aed927ab13995d7e575403a9d955ca
JarvisAdams <jarvis.hi.adams@gmail.com>
2019-07-19 16:35:01 -0400

    Still Merging

--- SQL/INSERTS.sql
+++ SQL/INSERTS.sql
@@ -1,3 +1,58 @@
 --insert new card
-INSERT INTO FC_Flashcards VALUES (cardID, front, back);
+--Question 1
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'To the nearest minute, how long does it take for light to travel from the Sun to the Earth?'
+,
+'8 Minutes'
+);
+--Question 2
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'The Spitfire originated from a racing plane.'
+,
+'True'
+);
+--Question 3
 
+--Question 4
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'Who was the first president of the United States?'
+,
+'George Washington'
+);
+--Question 5
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'HTML stands for:'
+,
+'Hypertext Markup Language'
+);
+
+--Question 6
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'The protagonist in the game "Cave Story" is named'
+,
+'Quote'
+);
+--Question 7
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'How many countries does Mexico border?'
+,
+'3'
+);
+--Question 8
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'Who won Big Brother 2014 UK?'
+,
+'Helen Wood'
+);
+--Question 9
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'In which year did "Caravan Palace" release their first album?'
+,
+'2008'
+);
+--Question 10
+INSERT INTO FC_Flashcard (front, back) VALUES (
+'Which stage was planned to be a part of "Sonic the Hedgehog 2", but was scrapped during development?'
+,
+'Genocide City Zone'
+);
